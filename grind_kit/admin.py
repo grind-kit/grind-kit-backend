24ffea32eb75b428c8a67b40df660b1e15764154
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-23 11:16:14 +0900

    Feat: Implement Jobs table, add firebase_uid and job_id columns to Users table

--- grind_kit/admin.py
+++ grind_kit/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import InstanceContent
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import InstanceContent, Job, Account
 
 admin.site.register(InstanceContent)
+admin.site.register(Job)
+
+class AccountInline(admin.StackedInline):
+    model = Account
+    can_delete = False
+    verbose_name_plural = 'UserProfiles'
+
+class CustomizedUserAdmin (UserAdmin):
+    inlines = (AccountInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, CustomizedUserAdmin)
--- grind_kit/models.py
+++ grind_kit/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class InstanceContent(models.Model):
@@ -8,3 +9,34 @@ class InstanceContent(models.Model):
     clear_exp = models.PositiveIntegerField(default=1)
     clear_gil = models.PositiveIntegerField(default=1)
     required_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    
+class Job (models.Model):
+    id = models.AutoField(primary_key=True)
+    pld_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    war_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    drk_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    gnb_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    whm_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    sch_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    ast_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    sge_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    mnk_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    drg_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    nin_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    sam_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    rpr_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    brd_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    mch_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    dnc_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    blm_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    smn_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+    rdm_level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)], null=True)
+
+class Account (models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firebase_uid = models.CharField(max_length=128)
+    job_id = models.ForeignKey(Job, on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.user.username
