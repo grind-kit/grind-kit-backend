50f4b2ad675221cb5e7a5caf33cf3e55a576fdb9
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-23 15:20:59 +0900

    Feat: Implement first POST endpoint

--- grind_kit/urls.py
+++ grind_kit/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', views.getRoutes, name='routes'),
     path('api/instancecontent/', views.getInstanceContent, name='instancecontent'),
-    path('api/job/<int:id>/', views.getJob, name='job')
+    path('api/job/<int:pk>/', views.jobList, name='job'),
 ]
--- grind_kit/views.py
+++ grind_kit/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from . serializers import *
@@ -22,8 +21,12 @@ def getInstanceContent (request):
     serializer = InstanceContentSerializer(content, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def getJob (request, id):
-    job = Job.objects.filter(id=id)
-    serializer = JobSerializer(job, many=True)
-    return Response(serializer.data)
+@api_view(['GET', 'POST'])
+def jobList (request, pk):
+    if request.method == 'GET':
+        job = Job.objects.filter(id=pk)
+        serializer = JobSerializer(job, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        return Response("Successful")
+        # job_data = JSONParser().
