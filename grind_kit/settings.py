3bdd4b380f3267bc925142836c33966853c45827
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-24 15:44:46 +0900

    Fix: Add Heroku app to allowed hosts

--- grind_kit/settings.py
+++ grind_kit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x*%4+clw#d@_xc5g#$b=xvg=x0dy8z9q6+8*v8q2mkhu8c!372
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'gksystem.herokuapp.com/']
 
 
 # Application definition
