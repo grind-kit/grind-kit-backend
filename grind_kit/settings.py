244ddd88cff9636e568aa191a94c7119f67506db
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-24 16:09:39 +0900

    Fix: Add Heroku DB settings

--- grind_kit/settings.py
+++ grind_kit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,6 +85,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
