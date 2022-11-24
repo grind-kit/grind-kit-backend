7ce373b43e782e31a8296e735e9680b38821ae44
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-24 15:29:58 +0900

    Feat: Implement requirements.txt file

--- grind_kit/settings.py
+++ grind_kit/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x*%4+clw#d@_xc5g#$b=xvg=x0dy8z9q6+8*v8q2mkhu8c!372'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- grind_kit/urls.py
+++ grind_kit/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +27,8 @@ urlpatterns = [
     path('api/instancecontent/', views.GetInstanceContent, name='instancecontent'),
     path('api/jobs/', views.JobsList, name='jobslist'),
     path('api/jobs/<int:pk>', views.JobsDetail, name='jobsdetail'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
+
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
