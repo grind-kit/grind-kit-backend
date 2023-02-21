cce57e0d7d66b60f3689f50891608e41d0be228b
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-02-21 16:03:41 +0900

    Fix: Re-register admin site models

--- grind_kit/admin.py
+++ grind_kit/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import gettext_lazy as _
+from .models import *
 
 from .models import User
 
@@ -25,3 +26,7 @@ class UserAdmin(DjangoUserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
+
+
+admin.site.register(InstanceContent) #Instance contents Model
+admin.site.register(Job) #Jobs Model
