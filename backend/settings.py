f4ee8f3e7b4f8a738f84e1b4042bff552bd46573
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-03-16 10:38:34 +0900

    Chore: Clean up requirements.txt

--- backend/settings.py
+++ backend/settings.py
@@ -79,7 +79,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
 
--- backend/urls.py
+++ backend/urls.py
@@ -4,9 +4,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('dj_rest_auth.urls')), #Endpoints provided by dj-rest-auth
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/social/login/', include('authentication.urls')) #Endpoint for Social logins
 ]
 
