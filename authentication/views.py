f18b8679eb8433203964f3911b9717c4213ee351
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-03-22 22:59:13 +0900

    Feat: Initialize exceptions file

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -8,7 +9,13 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
 
-        token['username'] = user.username
+        # Add custom claims
+        token["sub"] = settings.SIMPLE_JWT.get("ISSUER", "")
+        # When the serializer is called token['exp'] does not reflect the settings.ACCES_TOKEN_LIFETIME
+        # and is set to now + 1day,thus we subtract a day to get iat
+        token["iat"] = token["exp"] - (60 * 60 * 24)
+        # Add additional claims here
+        token["claims"] = {"is_superuser": user.is_superuser, "is_staff": user.is_staff}
 
         return token
     
