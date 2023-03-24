e5bdd5b2b68ff157691aae299397b6f7b6379847
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-03-24 14:40:19 +0900

    Feat: Initialize create_user view

--- api/views.py
+++ api/views.py
@@ -1,13 +1,19 @@
 from django.http import JsonResponse
-from rest_framework.response import Response
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 
-@api_view(['GET'])
-def my_view(request):
-    print("OK")
-    return HttpResponse("Hello, World!")
+@api_view(['POST'])
+def create_user(request):
+    uid = request.data.get('uid')
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+    user = User.objects.create_user(uid=uid, email=email, password=password)
+    user.save()
+
+    return Response({'message': 'User created successfully'})
 
 
 @api_view(['GET'])
