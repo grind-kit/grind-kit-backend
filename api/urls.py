d1ef2da085d7f0cbc2929af4ef2d503649e9323c
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-05-11 21:55:01 +0900

    Fix: Add create_bookmark view to urls listing

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.get_routes),
     path('users', views.create_user, name='create_user'),
     path('users/<str:username>', views.user_info_view, name='user_info_view'),
+    path('users/<int:user_id>/bookmarks', views.create_bookmark, name='create_bookmark'),
     path('conditions', views.get_content_finder_conditions, name='get_content_finder_conditions'),
 ]
