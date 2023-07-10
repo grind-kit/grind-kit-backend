8a52715a73a191c53e7e1bd16d58c4bdaaa736d4
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-07-10 11:28:04 +0900

    Feat: Touch api/fixtures directory

--- api/tests.py
+++ api/tests.py
@@ -2,21 +2,16 @@ from django.test import TestCase
 from firebase_admin import auth
 from decouple import config
 
-uuid = config('FIREBASE_ADMIN_UUID')
-custom_token = auth.create_custom_token(uuid)
 
 class ConditionsEndpointTest(TestCase):
-    def test_get_conditions_authenticated(self):
+    def test_get_conditions_success(self):
         query_params = {
             'type': '4',
-            'min': '1',
-            'max': '50'
-        }
-        headers = {
-            'Authorization': f'Bearer {custom_token}'
+            'min': '50',
+            'max': '60'
         }
 
-        response = self.client.get('/api/conditions/', headers=headers, data=query_params)
-        print(response.content)
+        response = self.client.get('/api/conditions/', data=query_params)
+        print(response.json())
         
         self.assertEqual(response.status_code, 200)
