c77c8667e40628511dbeb1b06ad5eead7e0d64a2
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-07-18 17:44:19 +0900

    Fix: Clean up missing b variable warnings in docker-compose

--- backend/settings.py
+++ backend/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_NAME', default='postgres'),
+        'USER': config('POSTGRES_USER', default='postgres'),
+        'PASSWORD': config('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': config('DB_HOST', default='db'),
+        'PORT': config('DB_PORT', default=5432),
     }
 }
 
